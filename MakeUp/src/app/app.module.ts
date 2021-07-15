e3c062b712f327af6d4d3337801a4b655d3d06a5
David <daviddelpozocalderon@gmail.com>
2021-07-15 20:14:37 +0200

    Configuración inicial
    Componentes, servicios y Boostrap

---
+++ MakeUp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListadoComponent } from './componentes/listado/listado.component';
+import { DetalleComponent } from './componentes/detalle/detalle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoComponent,
+    DetalleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
